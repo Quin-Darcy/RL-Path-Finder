403cd9e98a9caf6ff077b654b874942635525fe5
Quin Darcy <quintondarcy@csus.edu>
2021-11-25 21:33:23 +0000

    adjusted the progress graph to accentuate the data

--- utils.js
+++ utils.js
@@ -85,7 +85,13 @@ function update_progress() {
     let x = map(iter_num/iters, 0, 1, X_PAD, W-X_PAD);
     let p = abs(avg_len - min_len);
     let q = max_len - min_len;
-    let y = map(1-p/q, 0.8, 1, H-Y_PAD, Y_PAD);
+    let y = map(1-p/q, 0.8, 0.95, H-Y_PAD, Y_PAD);
+    if(y < Y_PAD) {
+        y = Y_PAD;
+    }
+    if (y > H-Y_PAD) {
+        y = H-Y_PAD;
+    }
     progress.push(createVector(x, y));
 }
 
@@ -93,7 +99,7 @@ function plot_progress() {
     for (let i = 0; i < progress.length; i++) {
         noStroke();
         fill(num_of_layers/2, 0.4, 1);
-        ellipse(progress[i].x, progress[i].y, 2);
+        ellipse(progress[i].x, progress[i].y, 1.5);
     }
 }
 
